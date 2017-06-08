e2b76c1eec5e8bcf3e96da5a3c959e9ea0e51884
Robert Nall <rmn0001@gmail.com>
2017-06-08 10:02:55 -0500

    fix column shift on csv export

--- overlay/recursive_intersect.R
+++ overlay/recursive_intersect.R
@@ -19,12 +19,12 @@ get_ins<-function(file.name){
   shp<-tryCatch(readOGR(file.name), error=function(s) NA)
   shp<-tryCatch(shp[lau, ], error=function(sj) NA)
   shp.df<-as.data.frame(shp)
-  #shp.df$source<-split[[1]][6]
+  #shp.df$source<-file_path_sans_ext(split[[1]][6])
   if(dir.exists(paste("geodata/clipped/", split[[1]][5], sep=""))){
     } else {
       dir.create(paste("geodata/clipped/", split[[1]][5], sep=""))
     }
-  write.csv(shp.df, paste("geodata/clipped/", split[[1]][5], "/", file_path_sans_ext(split[[1]][6]), ".csv", sep=""), row.names = F, quote = FALSE)
+  write.csv(shp.df, paste("geodata/clipped/", split[[1]][5], "/", file_path_sans_ext(split[[1]][6]), ".csv", sep=""), row.names = F, quote = TRUE)
   counter <<- counter + 1
   message(paste(counter, "of", total))
 }
