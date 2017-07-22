b319ff329a8057eeefae597bcf9f2ad6f12c581f
Robert Nall <rmn0001@gmail.com>
2017-07-22 17:18:58 -0500

    Add notes

--- xml/xml_to_table.R
+++ xml/xml_to_table.R
@@ -2,18 +2,18 @@ library(XML)
 library(RSQLite)
 library(sqldf)
 
-system.time(doc<-xmlParse("data/xml_data.xml", useInternalNodes = TRUE))
-els <- getNodeSet(doc, "//Table1")
+system.time(doc<-xmlParse("data/xml_data.xml", useInternalNodes = TRUE)) #load xml file
+els <- getNodeSet(doc, "//Table1") #identify index of nodes
 
-colm<-xmlToDataFrame(els[1])
-colm<-colm[0,]
-db<-dbConnect(SQLite(), dbname = "Output.sqlite")
-dbWriteTable(conn = db, name = "TableName", value = colm)
-dbDisconnect(db)
+colm<-xmlToDataFrame(els[1]) #create dummy dataframe
+colm<-colm[0,] #remove all rows, only keeping column names
+db<-dbConnect(SQLite(), dbname = "Output.sqlite") #create SQLite database
+dbWriteTable(conn = db, name = "TableName", value = colm) #write empty table to SQLite for proper columns
+dbDisconnect(db) #disconnect from SQLit
 
 
-counter<-0
-tot<-length(els)
+counter<-0 #counter setup for the iteration
+tot<-length(els) #get total number of nodes/elements
 system.time(for (i in 1:length(els)){
   node_gather.df<-xmlToDataFrame(els[i])
   db<-dbConnect(SQLite(), dbname = "Output.sqlite")
@@ -21,7 +21,7 @@ system.time(for (i in 1:length(els)){
   dbDisconnect(db)
   counter <<- counter + 1;
   message(paste(counter, "of", tot))
-})
+}) #iterate through each node, convert to a data frame, connect to SQLite, append dataframe to SQLite table, disconnect from SQLite, print a tracking number
 
 # counter<-0
 # tot<-length(els)
